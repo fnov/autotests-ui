4a78981340be351369b3a20537b030ce59121f8e
sm1Le <sm1Le@outlook.com>
2026-06-18 17:02:52 +0300

    6.5 Фикстуры Pytest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,9 @@
 import pytest
-from playwright.sync_api import sync_playwright, Page  # Имопртируем класс страницы для аннотации типов
+from playwright.sync_api import Page, Playwright  # Имопртируем класс страницы для аннотации типов
 
 
 @pytest.fixture  # Объявляем фикстуру, по умолчанию скоуп function
-def chromium_page() -> Page:
-    with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=False)
-        # Передаем страницу для использования в тесте
-        yield browser.new_page()
+def chromium_page(playwright: Playwright) -> Page:
+    browser = playwright.chromium.launch(headless=False)
+    # Передаем страницу для использования в тесте
+    yield browser.new_page()
