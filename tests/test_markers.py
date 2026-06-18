a8fd1aee6c733cf1f9ad5e298f9c5546692e44c8
sm1Le <sm1Le@outlook.com>
2026-06-18 12:29:01 +0300

    6.3 Маркировки Pytest

---
+++ tests/test_markers.py
@@ -0,0 +1,44 @@
+import pytest
+
+
+@pytest.mark.smoke
+@pytest.mark.regression
+class TestSuite:
+
+    def test_smoke_case(self):
+        pass
+
+    def test_regression_case(self):
+        pass
+
+
+@pytest.mark.regression
+class TestUserAuthentication:
+
+    @pytest.mark.smoke
+    def test_login(self):
+        pass
+
+    @pytest.mark.slow
+    def test_password_reset(self):
+        pass
+
+    def test_logout(self):
+        pass
+
+
+@pytest.mark.ui
+class TestUserInterface:
+
+    @pytest.mark.smoke
+    @pytest.mark.critical
+    def test_login_button(self):
+        pass
+
+    @pytest.mark.regression
+    def test_forgot_password_link(self):
+        pass
+
+    @pytest.mark.smoke
+    def test_signup_form(self):
+        pass
