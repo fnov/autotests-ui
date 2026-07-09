f00fc1d064f3e7f2a668d55412a497639515e67e
sm1Le <sm1Le@outlook.com>
2026-07-09 20:42:49 +0300

    7.7 Наращивание тестовой базы и рефакторинг. Практикуемся в написании автотестов с использованием Page Object, Page Component, Page Factory

--- tests/courses/test_courses.py
+++ tests/courses/test_courses.py
@@ -1,3 +1,5 @@
+import re
+
 import pytest
 
 from pages.courses.courses_list_page import CoursesListPage
@@ -35,3 +37,66 @@ class TestCourses:
         courses_list_page.course_view.check_visible(
             index=0, title='Playwright', max_score='100', min_score='10', estimated_time='2 weeks'
         )
+
+    def test_edit_course(self, create_course_page: CreateCoursePage, courses_list_page: CoursesListPage):
+        # Так хотелось написать
+        # self.test_create_course(create_course_page, courses_list_page)
+        # но, пожалуй, первые 3 пункта задания реализую явно
+
+        index = 0
+        title = 'Playwright'
+        new_title = 'Python'
+        estimated_time = '2 weeks'
+        new_estimated_time = '1 month'
+        description = 'Playwright'
+        new_description = 'Python'
+        max_score = '100'
+        new_max_score = '10'
+        min_score = '10'
+        new_min_score = '1'
+
+        create_course_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create")
+
+        create_course_page.image_upload_widget.upload_preview_image('./testdata/files/image.png')
+        create_course_page.image_upload_widget.check_visible(is_image_uploaded=True)
+        create_course_page.create_course_form.fill(
+            title=title,
+            estimated_time=estimated_time,
+            description=description,
+            max_score=max_score,
+            min_score=min_score
+        )
+        create_course_page.create_course_toolbar.click_create_course_button()
+
+        courses_list_page.check_current_url(re.compile(".*#/courses"))
+        courses_list_page.course_view.check_visible(
+            index=index,
+            title=title,
+            estimated_time=estimated_time,
+            max_score=max_score,
+            min_score=min_score
+        )
+
+        courses_list_page.course_view.menu.click_edit(index=0)
+        create_course_page.create_course_form.fill(
+            title=new_title,
+            estimated_time=new_estimated_time,
+            description=new_description,
+            max_score=new_max_score,
+            min_score=new_min_score
+        )
+        create_course_page.create_course_toolbar.click_create_course_button()
+
+        courses_list_page.check_current_url(re.compile(".*#/courses"))
+        courses_list_page.course_view.check_visible(
+            index=index,
+            title=new_title,
+            estimated_time=new_estimated_time,
+            max_score=new_max_score,
+            min_score=new_min_score
+        )
+
+        # Отдельная проверка description, так как его нет в карточке курса
+        courses_list_page.course_view.menu.click_edit(index=0)
+        create_course_page.create_course_form.description_textarea.check_have_value(new_description)
+        create_course_page.create_course_toolbar.click_create_course_button()
