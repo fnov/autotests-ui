8a70a237287f087891fec775f30365e01788d56b
sm1Le <sm1Le@outlook.com>
2026-07-18 15:03:23 +0300

    9.2 Запуск автотестов на разных браузерах. Практическое задание: запуск автотестов на нескольких браузерах

--- tools/playwright/pages.py
+++ tools/playwright/pages.py
@@ -15,8 +15,9 @@ def initialize_playwright_page(
     browser = playwright[browser_type].launch(headless=settings.headless)
     context = browser.new_context(
         base_url=settings.get_base_url(),
-        storage_state=storage_state,
-        record_video_dir=settings.videos_dir
+        storage_state=storage_state
+        # Временно отключена запись видео из-за ошибок webkit и снижения размера отчета
+        # record_video_dir=settings.videos_dir
     )
     context.tracing.start(screenshots=True, snapshots=True, sources=True)
     page = context.new_page()
@@ -27,4 +28,5 @@ def initialize_playwright_page(
     browser.close()
 
     allure.attach.file(settings.tracing_dir.joinpath(f'{test_name}.zip'), name='trace', extension='zip')
-    allure.attach.file(page.video.path(), name='video', attachment_type=allure.attachment_type.WEBM)
+    # Временно отключена запись видео из-за ошибок webkit и снижения размера отчета
+    # allure.attach.file(page.video.path(), name='video', attachment_type=allure.attachment_type.WEBM)
