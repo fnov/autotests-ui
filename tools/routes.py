5488b4d5287cfc06eaf1c936a28096edc89b5dfe
fnov <email@for.this.repo.com>
2026-07-23 19:10:45 +0300

    10.4 Запуск автотестов на GitHub Actions

--- tools/routes.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-
-class AppRoute(str, Enum):
-    LOGIN = "./#/auth/login"
-    REGISTRATION = "./#/auth/registration"
-    DASHBOARD = "./#/dashboard"
-    COURSES_LIST = "./#/courses"
-    COURSES_CREATE = "./#/courses/create"
